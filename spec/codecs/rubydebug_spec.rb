4ca24fb9cba4bd885b3285e9f7b3fb33ef189705
Tal Levy <JubBoy333@gmail.com>
2014-11-03 16:14:43 -0800

    add spec

---
+++ spec/codecs/rubydebug_spec.rb
@@ -0,0 +1,31 @@
+require "logstash/codecs/rubydebug"
+require "logstash/event"
+require "awesome_print"
+require "insist"
+
+describe LogStash::Codecs::RubyDebug do
+
+
+  subject do
+    next LogStash::Codecs::RubyDebug.new
+  end
+
+  context "#encode" do
+    it "should print beautiful hashes" do
+      test_event = LogStash::Event.new({"what" => "ok", "who" => 2})
+      got_event = false
+      subject.on_event do |d|
+        insist { d.chomp } == test_event.to_hash.awesome_inspect 
+        got_event = true
+      end
+      subject.encode(test_event)
+      insist { got_event }
+    end
+  end
+
+  context "#decode" do
+    it "should not be implemented" do
+      expect { subject.decode("data") }.to raise_error("Not implemented")
+    end
+  end
+end
