237792a447e8507586ee8472a2a14d150097e9fe
Joao Duarte <jsvduarte@gmail.com>
2017-07-25 11:05:23 +0100

    remove references to rubydebug since rename to pretty

--- lib/logstash/codecs/pretty.rb
+++ lib/logstash/codecs/pretty.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 require "logstash/codecs/base"
 
-# The rubydebug codec will output your Logstash event data using
+# The pretty codec will output your Logstash event data using
 # the Ruby Awesome Print library.
 #
 class LogStash::Codecs::Pretty < LogStash::Codecs::Base
